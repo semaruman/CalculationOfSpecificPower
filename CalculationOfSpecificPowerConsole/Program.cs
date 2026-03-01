9d36dfd40d7392dc6d753bce05f6153022202948
Semen <semen.ruman@gmail.com>
2026-03-01 13:39:12 +0300

    Добавил вычисление тока, момента, % потерь в главное окно

--- CalculationOfSpecificPowerConsole/Program.cs
+++ CalculationOfSpecificPowerConsole/Program.cs
@@ -4,8 +4,6 @@ public class Program
 {
     public static void Main()
     {
-        
-
         Console.WriteLine("Введите количество потребителей:");
 
         int consCount = Convert.ToInt32(Console.ReadLine());
@@ -14,7 +12,26 @@ public class Program
 
         var specPower = PowerCalculator.CalculateSpecificPower((int)dataList[0], (int)dataList[1], (int)dataList[2], dataList[3], dataList[4]);
         var fullspecPower = PowerCalculator.CalculateFullSpecificPower(consCount, specPower);
+        var tok = PowerCalculator.CalculateTok(fullspecPower);
+
         Console.WriteLine($"На одну квартиру: {specPower}");
         Console.WriteLine($"На все квартиры: {fullspecPower}");
+        Console.WriteLine($"Ток: {tok}");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Введите длину ВЛ метров:");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        var moment = PowerCalculator.CalculateMoment(length, fullspecPower);
+
+        Console.WriteLine($"Момент: {moment}");
+
+        Console.WriteLine("Введите сечение");
+        double section = Convert.ToDouble(Console.ReadLine());
+
+        var lossesPercent = PowerCalculator.CalculateLossesPercent(moment, section);
+
+        Console.WriteLine($"% потерь: {lossesPercent}");
     }
 }
