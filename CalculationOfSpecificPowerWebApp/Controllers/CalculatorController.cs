d99079cf2bbff8597f436230cbef7a8cebf0cf2d
Semen <semen.ruman@gmail.com>
2026-04-01 07:28:20 +0300

    Добавил меню эндпоинтов для api по расчётам

---
+++ CalculationOfSpecificPowerWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculationOfSpecificPowerWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        
+    }
+}
--- CalculationOfSpecificPowerWebApp/Program.cs
+++ CalculationOfSpecificPowerWebApp/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
+app.MapGet("/", GetMenu);
 app.Run();
+
+IResult GetMenu()
+{
+    return Results.Ok(new
+    {
+        Endpoints = new[]
+        {
+            "GET: /api/calculator/specific-power?count={количество потребителей}&type={тип потребител€} - удельна€ мощность, Pуд",
+            "GET: /api/calculator/rated-power?count={количество потребителей}&type={тип потребител€} - расчЄтна€ мощность, Pp",
+            "GET: /api/calculator/electric-current?count={количество потребителей}&type={тип потребител€}&cosf={0.98 - п.у.} - сила тока, Ip",
+            "GET: /api/calculator/moment?count={количество потребителей}&type={тип потребител€}&length={длина ЋЁѕ метров} - момент",
+        }
+    });
+}
