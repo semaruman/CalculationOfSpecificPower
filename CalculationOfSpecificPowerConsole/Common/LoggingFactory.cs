76918c68deb0f40052f1413b01d313df98cb229d
Semen <semen.ruman@gmail.com>
2026-04-07 06:40:15 +0300

    Добавил логгирование при расчётах величин в PowerCalculator

---
+++ CalculationOfSpecificPowerConsole/Common/LoggingFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationOfSpecificPowerConsole.Common
+{
+    public static class LoggingFactory
+    {
+        private static readonly ILoggerFactory _factory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.AddDebug();
+        });
+
+        public static ILogger CreateLogger(string s) => _factory.CreateLogger(s);
+    }
+}
--- CalculationOfSpecificPowerConsole/Common/PowerCalculator.cs
+++ CalculationOfSpecificPowerConsole/Common/PowerCalculator.cs
@@ -3,51 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace CalculationOfSpecificPowerConsole.Common
 {
-    public class PowerCalculator
+    public static class PowerCalculator
     {
+        private static readonly ILogger _logger = LoggingFactory.CreateLogger("PowerCalculator");
         public static double CalculateSpecificPower(int currentFlat, int flatMin, int flatMax, double kilowattMin, double kilowattMax)
         {
             double kilowattDefference = kilowattMax - kilowattMin;
             int flatDefference = flatMax - flatMin;
             int currentFlatDef = currentFlat - flatMin;
+            double specificPower = kilowattMax - kilowattDefference / flatDefference * currentFlatDef;
 
-            return kilowattMax - kilowattDefference / flatDefference * currentFlatDef;
+            _logger.LogInformation("Удельная мощность: {spec}", specificPower);
+
+            return specificPower;
         }
 
         public static double CalculateFullSpecificPower(int currentFlat, int flatMin, int flatMax, double kilowattMin, double kilowattMax)
         {
-            return CalculateSpecificPower(currentFlat, flatMin, flatMax, kilowattMin, kilowattMax) * currentFlat;
+            double fulSpecificPower = CalculateSpecificPower(currentFlat, flatMin, flatMax, kilowattMin, kilowattMax) * currentFlat;
+
+            _logger.LogInformation("Полная мощность: {spec}", fulSpecificPower);
+
+            return fulSpecificPower;
         }
 
         public static double CalculateFullSpecificPower(int currentFlat, double specificPower)
         {
-            return specificPower * currentFlat;
+            double fulSpecificPower = specificPower * currentFlat;
+
+            _logger.LogInformation("Полная мощность: {spec}", fulSpecificPower);
+
+            return fulSpecificPower;
         }
 
 
         // Вычисление момента по длине ВЛ метров
         public static double CalculateMoment(double lenght, double fullSpecificPower)
         {
-            return lenght * fullSpecificPower;
+            double moment = lenght * fullSpecificPower;
+
+            _logger.LogInformation("Момент: {moment}", moment);
+
+            return moment;
         }
 
         public static double CalculateMoment(double lenght, int currentFlat, int flatMin, int flatMax, double kilowattMin, double kilowattMax)
         {
-            return lenght * CalculateFullSpecificPower(currentFlat, flatMin, flatMax, kilowattMin, kilowattMax);
+            double moment = lenght * CalculateFullSpecificPower(currentFlat, flatMin, flatMax, kilowattMin, kilowattMax);
+
+            _logger.LogInformation("Момент: {moment}", moment);
+
+            return moment;
         }
 
         // Вычисление % потерь по моменту и сечению мм.кв.
         public static double CalculateLossesPercent(double moment, double sectionMMkw)
         {
-            return moment / sectionMMkw / 46;
+
+            double lossesPercent = moment / sectionMMkw / 46;
+
+            _logger.LogInformation("Процент потерь: {lossesPercent}", lossesPercent);
+
+            return lossesPercent;
         }
 
         public static double CalculateTok(double fullSpecificPower, double cosF = 0.98)
         {
-            return fullSpecificPower / 0.38 / cosF / Math.Sqrt(3);
+            double tok = fullSpecificPower / 0.38 / cosF / Math.Sqrt(3);
+
+            _logger.LogInformation("Ток: {tok}", tok);
+
+            return tok;
         }
     }
 }
