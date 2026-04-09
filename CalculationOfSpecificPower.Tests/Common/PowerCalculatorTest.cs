6a4fed91d3bdf55fc1b695bf55eec63d9cd78373
Semen <semen.ruman@gmail.com>
2026-04-09 06:19:19 +0300

    добавил тест по расчёту удельной мощности

---
+++ CalculationOfSpecificPower.Tests/Common/PowerCalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculationOfSpecificPowerConsole.Common;
+
+namespace CalculationOfSpecificPowerConsole.Tests.Common
+{
+    public class PowerCalculatorTest
+    {
+        [Theory]
+        [InlineData(23, 18, 24, 1.4, 1.65)]
+        public void CalculateSpecificPower_ReturnsCorrectNumber(
+            int currentFlat, 
+            int flatMin, 
+            int flatMax, 
+            double kilowattMin, 
+            double kilowattMax)
+        {
+            double kilowattDefference = kilowattMax - kilowattMin;
+            int flatDefference = flatMax - flatMin;
+            int currentFlatDef = currentFlat - flatMin;
+
+            double correctSpecificPower = kilowattMax - kilowattDefference / flatDefference * currentFlatDef;
+            double calculatorSpecificPower = PowerCalculator.CalculateSpecificPower(currentFlat, flatMin, flatMax, kilowattMin, kilowattMax);
+
+            Assert.Equal(correctSpecificPower, calculatorSpecificPower);
+        }
+    }
+}
